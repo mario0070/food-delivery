5d7c740ecbcbf102cb077193f5552399272dd1d4
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-01-16 07:44:41 +0100

    project setup

---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Nabar() {
+  return (
+    <div>Nabar</div>
+  )
+}
---
+++ src/pages/landing_page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "/public/css/landing.css"
+
+export default function LandingPage() {
+  return (
+    <div>Landing Page</div>
+  )
+}
