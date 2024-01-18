42d7ea23f443876d2509e7bbef7222110c1badce
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-01-18 20:41:06 +0100

    vendor dashboard

--- src/pages/vendor_dashboard.jsx
+++ src/pages/vendor_dashboard.jsx
@@ -16,8 +16,20 @@ export default function VendorDashboard() {
   return (
     <div className='vendor_dashboard d-flex'>
       <div className="sidebar">
-
+          <div className="top">
+            <h2 className='p-2 mb-4 text-white'><a href="/">Swift</a></h2>
+            <div className="links">
+              <p className=""><a href="" className='active'><i class="fa-solid fa-house"></i> Dashboard</a></p>
+              <p className=""><a href=""><i class="fa-solid fa-comments"></i> Chat</a></p>
+              <p className=""><a href=""><i class="fa-brands fa-product-hunt"></i> Products</a></p>
+              <p className=""><a href=""><i class="fa-solid fa-chart-simple"></i> Analytic</a></p>
+              <p className=""><a href=""><i class="fa-brands fa-first-order-alt"></i> All Orders</a></p>
+              <p className=""><a href=""><i class="fa-solid fa-user"></i> Profile</a></p>
+              <p className=""><a href=""><i class="fa-solid fa-arrow-right-from-bracket"></i> Log Out</a></p>
+            </div>
+          </div>
       </div>
+
       <div className="main_content">
         <div className="topbar d-flex">
             <div className="d-flex">
