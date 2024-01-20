75f53b77108526c82c186745560a9d5397f5c7d0
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-01-20 19:31:46 +0100

    product

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -3,9 +3,12 @@ import Sidebar from '../components/side_bar'
 import Topbar from '../components/topbar'
 import "/public/css/product.css"
 import banner3 from "/img/home-banner3.jpg"
+import phone from "/img/phone1.jpeg"
+import packages from "/img/Packages.jpg"
 
 export default function Products() {
     const [role, setRole] = useState("Vendor")
+    const [show, setShow] = useState(false)
 
     const toggle = () => {
         const topbar = document.querySelector(".topbar")
@@ -14,7 +17,59 @@ export default function Products() {
         topbar.classList.toggle("active")
         main_content.classList.toggle("active")
         sidebar.classList.toggle("active")
-      }
+    }
+
+    const alert = () => {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "info",
+            title: "under development"
+          });
+    }
+
+    const deleteProduct = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonColor: "#2a3042",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your product has been deleted.",
+                icon: "success"
+              });
+            }
+        });
+    }
+
+    const showForm = () => {
+        var form = document.querySelector(".form")
+        var add = document.querySelector(".add")
+        form.style.display = "inline"
+        add.style.display = "none"
+    }
+
+    const closeForm = () => {
+        var form = document.querySelector(".form")
+        var add = document.querySelector(".add")
+        form.style.display = "none"
+        add.style.display = "inline"
+    }
 
     return (
         <div className='vendor_dashboard'>
@@ -24,30 +79,62 @@ export default function Products() {
                 <Topbar toggle={toggle}/>
 
                 <div className="products special d-flex">
-                    <div className="box">
+                    <div className="box add text-center">
+                       <p className="text-muted btn" onClick={showForm}><i class="fa-solid fa-plus"></i></p>
+                       <p className="p">Add Product</p>
+                    </div>
 
+                    <div className="box form">
+                        <input type="text" className='mt-3'  placeholder='Product Name'/>
+                        <input type="text"  placeholder='Price'/>
+                        <input type="text"  placeholder='Description'/>
+                        <label htmlFor="file">Choose product image <i class="fa-solid fa-camera"></i></label>
+                        <input type="file" className='d-none' name="file" id="file" />
+                        <button className='btn' onClick={alert}>Add Product</button>
+                        <p className="text-muted arrow btn" onClick={closeForm}><i class="fa-solid fa-arrow-left"></i></p>
+                    </div>
+
+                    <div className="box">
+                        <img src={phone} alt="" />
+                        <div className="text p-3">
+                            <p className="fw-bold mb-0">Iphone 29 pro</p>
+                            <p className="text-muted info">brand new iphone 29 pro with....</p>
+                            <p className="text-danger btn" onClick={deleteProduct}><i class="fa-solid fa-trash"></i></p>
+                            <h4 className='fw-bold mny'>₦200,000</h4>
+                        </div>
                     </div>
                     <div className="box">
-
+                        <img src={packages} alt="" />
+                        <div className="text p-3">
+                            <p className="fw-bold mb-0">Iphone 29 pro</p>
+                            <p className="text-muted info">brand new iphone 29 pro with....</p>
+                            <p className="text-danger btn" onClick={deleteProduct}><i class="fa-solid fa-trash"></i></p>
+                            <h4 className='fw-bold mny'>₦200,000</h4>
+                        </div>
                     </div>
                     <div className="box">
-
+                        <img src={phone} alt="" />
+                        <div className="text p-3">
+                            <p className="fw-bold mb-0">Iphone 29 pro</p>
+                            <p className="text-muted info">brand new iphone 29 pro with....</p>
+                            <p className="text-danger btn" onClick={deleteProduct}><i class="fa-solid fa-trash"></i></p>
+                            <h4 className='fw-bold mny'>₦200,000</h4>
+                        </div>
                     </div>
                     <div className="box">
-
+                        <img src={phone} alt="" />
+                        <div className="text p-3">
+                            <p className="fw-bold mb-0">Iphone 29 pro</p>
+                            <p className="text-muted info">brand new iphone 29 pro with....</p>
+                            <p className="text-danger btn" onClick={deleteProduct}><i class="fa-solid fa-trash"></i></p>
+                            <h4 className='fw-bold mny'>₦200,000</h4>
+                        </div>
                     </div>
-                    <div className="box">
+                   
+                </div>
 
-                    </div>
-                    <div className="box">
-
-                    </div>
-                    <div className="box">
-
-                    </div>
-                    <div className="box">
-
-                    </div>
+                <div className="section3 mt-4">
+                    <p className="text-center text-white">© Copyright 2016, All rights reserved</p>
                 </div>
             </div>
         </div>
