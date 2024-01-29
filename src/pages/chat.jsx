bd9714334b8d9baffc13f92d5d790c9be75fb2c3
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-01-29 17:57:33 +0100

    fetching chat

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -1,16 +1,21 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Sidebar from '../components/side_bar'
 import Topbar from '../components/topbar'
 import "/public/css/chat.css"
 import banner3 from "/img/home-banner3.jpg"
 import $ from 'jquery';
 import { useCookies } from 'react-cookie'
+import axios from '../utils/axios'
 
 export default function Chat() {
   const msgInputs = useRef("")
   const [role, setRole] = useState("Vendor")
   const [cookie, setCookie, removeCookie] = useCookies("")
   const [user, setUser] = useState(cookie.user ?? "")
+  const [chat, setchat] = useState([])
+  const [agent, setagent] = useState([])
+  const [loaded, setloaded] = useState(true)
+  const [recName, setrecName] = useState("Start Conversation")
 
   const toggle = () => {
     const topbar = document.querySelector(".topbar")
@@ -58,129 +63,178 @@ export default function Chat() {
     }
   }
 
-  return (
-    <div className='vendor_dashboard'>
-      <Sidebar role={role}/>
+  useEffect(() => {
+    axios.get("/user", {
+        id : user._id
+    }).then(res => {
+        setagent(res.data.users)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+  },[agent])
 
-      <div className="main_content">
-        <Topbar toggle={toggle}/>
-        
-        <div className="chat special d-flex">
-            <div className="all_chats">
-              <h4 className='fw-bold'>Chat</h4>
-              <div className="user mt-5 d-flex">
-                <div className="d-flex">
-                  <img src={banner3} alt="" />
-                    <div className="mx-3">
-                      <p className="mb-0 fw-bold text-capitalize">{user.lastname} {user.firstname}</p>
-                      <div className="d-flex">
-                        <p className='dot mb-0'></p>
-                        <p className="mb-0">Active</p>
-                      </div>
-                    </div>
-                </div>
-                <div className="">
-                  <i class="fa-solid fa-bell text-danger"></i>
-                  </div>
-              </div>
+  const fetchMsg = (recipient, name) => {
+    setrecName(name)
+    setloaded(false)
+    axios.post("/chat/conversation", {
+      recipient : recipient,
+      sender : user._id
+    }).then(res => {
+        setloaded(true)
+        setchat(res.data.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+  }
 
-              <div className="input mt-5 mb-4">
-                <input type="text" name="" id=""  placeholder='Search....'/>
-              </div>
+  if(cookie.user){
+    return (
+      <div className='vendor_dashboard'>
+        <Sidebar role={role}/>
 
-              <div className="recent">
-                <p className="mb-2 text-muted">Recent</p>
-                <div className="box">
-                  <div className="d-flex mt-2 single">
-                    <div className="d-flex">
-                        <p className="dot mb-0"></p>
-                        <img src={banner3} alt="" />
-                        <div className="mx-3">
-                          <p className="mb-0 fw-bold">Ganiu jamiu</p>
-                          <p className="msg">Hey, How are you?</p>
-                        </div>
-                    </div>
-                    <i class="fa-regular fa-message"></i>
-                  </div>
-                  {/* <div className="d-flex single">
-                    <div className="d-flex">
-                        <p className="dot mb-0"></p>
-                        <img src={banner3} alt="" />
-                        <div className="mx-3">
-                          <p className="mb-0 fw-bold">Ganiu jamiu</p>
-                          <p className="msg">How well is your service, i wanna try it out</p>
-                        </div>
-                    </div>
-                    <i class="fa-regular fa-message"></i>
-                  </div>
-                  <div className="d-flex single">
-                    <div className="d-flex">
-                        <p className="dot mb-0"></p>
-                        <img src={banner3} alt="" />
-                        <div className="mx-3">
-                          <p className="mb-0 fw-bold">Ganiu jamiu</p>
-                          <p className="msg">last message</p>
-                        </div>
-                    </div>
-                    <i class="fa-regular fa-message"></i>
-                  </div> */}
-                </div>
-              </div>
-
-            </div>
-            
-            <div className="chat_box">
-                <div className="head d-flex">
-                  <div className="">
-                    <p className="fw-bold mb-0 text-capitalize">{user.lastname} {user.firstname}</p>
-                    <div className="d-flex">
-                      <p className="dot mb-0"></p>
-                      <p className="mb-0">Active Now</p>
-                    </div>
-                  </div>
+        <div className="main_content">
+          <Topbar toggle={toggle}/>
+          
+          <div className="chat special d-flex">
+              <div className="all_chats">
+                <h4 className='fw-bold'>Chat</h4>
+                <div className="user mt-5 d-flex">
                   <div className="d-flex">
-                    <i class="fa-solid fa-bell"></i>
-                    <i class="fa-solid fa-info"></i>
-                    <i class="fa-solid fa-gears"></i>
+                    <img src={banner3} alt="" />
+                      <div className="mx-3">
+                        <p className="mb-0 fw-bold text-capitalize">{user.lastname} {user.firstname}</p>
+                        <div className="d-flex">
+                          <p className='dot mb-0'></p>
+                          <p className="mb-0">Active</p>
+                        </div>
+                      </div>
+                  </div>
+                  <div className="">
+                    <i class="fa-solid fa-bell text-danger"></i>
+                    </div>
+                </div>
+
+                <div className="input mt-5 mb-4">
+                  <input type="text" name="" id=""  placeholder='Search....'/>
+                </div>
+
+                <div className="recent">
+                  <p className="mb-2 text-muted">Recent</p>
+                  <div className="box">
+                    {user.role == "vendor" && agent.map((val, index) => {
+                      if(val.role != "vendor"){
+                        return (
+                            <div onClick={() => fetchMsg(val._id,`${val.firstname} ${val.lastname}`)} className="d-flex mt-2 single">
+                              <div className="d-flex">
+                                  <p className="dot mb-0"></p>
+                                  <img src={banner3} alt="" />
+                                  <div className="mx-3">
+                                    <p className="mb-0 fw-bold text-capitalize">{val.firstname}</p>
+                                    <p className="msg text-capitalize">{val.role}</p>
+                                  </div>
+                              </div>
+                              <i class="fa-regular fa-message"></i>
+                          </div>
+                        )
+                      }
+                    })}
+
+                    {user.role == "user" && agent.map((val, index) => {
+                      if(val.role != "user"){
+                        return (
+                            <div onClick={() => fetchMsg(val._id, `${val.firstname} ${val.lastname}`)} className="d-flex mt-2 single">
+                              <div className="d-flex">
+                                  <p className="dot mb-0"></p>
+                                  <img src={banner3} alt="" />
+                                  <div className="mx-3">
+                                    <p className="mb-0 fw-bold text-capitalize">{val.firstname}</p>
+                                    <p className="msg text-capitalize">{val.role}</p>
+                                  </div>
+                              </div>
+                              <i class="fa-regular fa-message"></i>
+                          </div>
+                        )
+                      }
+                    })}
+                    
                   </div>
                 </div>
 
-                <div className="msg-container" id='msgBody'>
-
-                  <div className="wrap1 pt-4">
+              </div>
+              
+              <div className="chat_box">
+                  <div className="head d-flex">
                     <div className="">
-                        <p className='mb-0 mx-3'><i class="fa-regular fa-user"></i></p>
-                        <div className="msgBodys mt-0">
-                            <p className='mb-0 p-2'>Hello jamiu, what questions do you have for me today? </p>
-                        </div>
-                    </div>
-                  </div>
-
-                  <div class="wrap2 mt -2">
-                    <p className='mb-0 text-end mx-3 msgIcon'><i class="fa-regular fa-user"></i></p>
-                    <div class="sentMsg mt-0">
-                      <div class="myMsg">
-                          <p class="mb-0 p-2">Hello jamiu, what questions do you have for me today? </p>
+                      <p className="fw-bold mb-0 text-capitalize">{recName}</p>
+                      <div className="d-flex">
+                        <p className="dot mb-0"></p>
+                        <p className="mb-0">Active Now</p>
                       </div>
                     </div>
+                    <div className="d-flex">
+                      <i class="fa-solid fa-bell"></i>
+                      <i class="fa-solid fa-info"></i>
+                      <i class="fa-solid fa-gears"></i>
+                    </div>
                   </div>
-                                   
-                </div>
 
-                <div className="sendBtn">
-                  <div className="d-flex mt-3">
-                    <textarea  ref={msgInputs} name="" id=""  placeholder='Type message.....'></textarea>
-                    <button className="btn send_msg text-white" onClick={sendMsg}>Send message</button>
+                  <div className="pt-4 msg-container" id='msgBody'>
+
+                    {
+                      !loaded && <div className='text-center mt-5'><div class="spinner-border text-dark"></div></div>
+                    }
+
+                    { loaded &&
+                      chat.map(val => {
+                        if(val != null){
+                          if(user._id == val.sender._id){
+                            return (
+                              <div class="wrap2 mt -2">
+                                <p className='mb-0 text-end mx-3 msgIcon'><i class="fa-regular fa-user"></i></p>
+                                <div class="sentMsg mt-0">
+                                  <div class="myMsg">
+                                      <p class="mb-0 p-2">{val.msg}</p>
+                                  </div>
+                                </div>
+                              </div> 
+                            )
+                          }else{
+                            return (
+                              <div className="wrap1 pt-4">
+                                <div className="">
+                                    <p className='mb-0 mx-3'><i class="fa-regular fa-user"></i></p>
+                                    <div className="msgBodys mt-0">
+                                        <p className='mb-0 p-2'>{val.msg}</p>
+                                    </div>
+                                </div>
+                              </div>
+                            )
+                          }
+                        }
+                      }).reverse()
+                    }
+                                    
                   </div>
-                </div>
-            </div>
-        </div>
 
-        <div className="section3 mt-4">
-          <p className="text-center text-white">© Copyright 2016, All rights reserved</p>
+                  <div className="sendBtn">
+                    <div className="d-flex mt-3">
+                      <textarea  ref={msgInputs} name="" id=""  placeholder='Type message.....'></textarea>
+                      <button className="btn send_msg text-white" onClick={sendMsg}>Send message</button>
+                    </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="section3 mt-4">
+            <p className="text-center text-white">© Copyright 2016, All rights reserved</p>
+          </div>
         </div>
+        
       </div>
-      
-    </div>
-  )
+    )
+  }else{
+    window.location.href = "/login"
+  }
 }
